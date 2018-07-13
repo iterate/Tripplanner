ef62131c7f63915f08bb97def2169b89c073b55c
Eirik Skjærseth <eirik@MacBook-Pro-6.local>
2018-07-13 13:40:28 +0200

    setup routing of front page and map page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import VisDatabaseTest from "./VisDatabaseTest";
 
 class App extends Component {
 	render() {
-		return (
-			<div className="App">
-				<FirebaseHandeler>
-					<VisDatabaseTest />
-				</FirebaseHandeler>
-			</div>
-		);
+		//get the path specified after the domain in the url.
+		//e.g. www.tripplanner.com/myCoolMap gives "/myCoolMap"
+		let roomId = window.location.pathname;
+
+		if (roomId === "/") {
+			//render landing screen
+			return "Front page!";
+		} else {
+			//render map and points according to the roomId
+			return (
+				<div className="App">
+					<FirebaseHandeler>
+						<VisDatabaseTest />
+					</FirebaseHandeler>
+				</div>
+			);
+		}
 	}
 }
 
