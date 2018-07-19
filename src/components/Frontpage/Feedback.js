cd1684f8ba62f1b0fd5e2e38fdc7babd05437ce4
Eirik Skjærseth <eirik@MacBook-Pro-6.local>
2018-07-19 15:23:57 +0200

    fix the mail address on the bottom of front page

--- src/components/Frontpage/Feedback.js
+++ src/components/Frontpage/Feedback.js
@@ -3,19 +3,19 @@ import styled from "styled-components";
 import Step from "./Step";
 
 const StyledStep = styled(Step)`
-  margin: auto;
-  color: white;
+	margin: auto;
+	color: white;
 `;
 
 const Feedback = ({ className }) => (
-  <StyledStep
-    className={className}
-    img={"/images/contact.png"}
-    stepHeading={"Contact us"}
-    spanTxt={
-      "If you need to get in contact with us, just hit us up on post@mappoint.com"
-    }
-  />
+	<StyledStep
+		className={className}
+		img={"/images/contact.png"}
+		stepHeading={"Contact us"}
+		spanTxt={
+			"If you need to get in contact with us, just hit us up on tripplanner@iterate.no"
+		}
+	/>
 );
 
 export default Feedback;
